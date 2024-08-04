701640019e0c10966155ac3dd34e3f214e8a4c98
Anjali <anjalisinha89871@gmail.com>
2024-08-04 18:33:22 +0530

    second commit

---
+++ src/Min_Steps_in_Infinite_Grid.java
@@ -0,0 +1,12 @@
+//Problem Source ==> https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
+
+import java.util.ArrayList;
+
+public class Min_Steps_in_Infinite_Grid {
+    public int coverPoints(ArrayList<Integer> A, ArrayList<Integer> B) {
+        int steps = 0;
+        for(int i =0;i<A.size()-1;i++)
+            steps += Math.max(Math.abs(A.get(i) - A.get(i+1)),Math.abs(B.get(i) - B.get(i+1)));
+        return steps;
+    }
+}
---
+++ src/Pick_from_both_sides.java
@@ -0,0 +1,22 @@
+//Problem Source ==> https://www.interviewbit.com/problems/pick-from-both-sides/
+
+import java.util.ArrayList;
+
+public class Pick_from_both_sides {
+    public int solve(ArrayList<Integer> A, int B) {
+        int N = A.size();
+        int Sum1 = 0;
+        int Sum2 = 0;
+        for (int i = 0; i < B; i++) {
+            Sum1 += A.get(i);
+        }
+        Sum2 = Sum1;
+        for (int i = B - 1, j = N - 1; i >= 0; i--, j--) {
+            Sum1 -= A.get(i);
+            Sum1 += A.get(j);
+            Sum2 = Math.max(Sum2, Sum1);
+        }
+
+        return Sum2;
+    }
+}
