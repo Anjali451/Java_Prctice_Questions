f69bfbd24884db459dd4b5c4ef077583fb6e8c1e
Anjali <anjalisinha89871@gmail.com>
2024-09-22 19:09:57 +0530

    new questions

---
+++ src/BinarySearch/Search_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,37 @@
+//Problem Source ==> https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+package BinarySearch;
+
+public class Search_in_Rotated_Sorted_Array_II {
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left <=right){
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target){
+                return true;
+            }else if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+                right -=1;
+                left +=1;
+            }else if(nums[mid] >= nums[left]){
+                if(nums[left] <=target && target <= nums[mid]){
+                    right = mid -1;
+                }else{
+                    left= mid +1;
+                }
+            }else{
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid +1;
+                }
+                else{
+                    right = mid -1;
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
